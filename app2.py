4f1fd9b85b1da9d58234fa24b3822100ddc5d7bb
Tatsuro-0414 <83904503+Tatsuro-fastball14@users.noreply.github.com>
2023-06-20 07:02:21 +0900

    bootstrap　
    bootstrap

--- app2.py
+++ app2.py
@@ -2,9 +2,8 @@
 from flask import Flask, render_template
 import pandas as pd 
 df=pd.read_csv("/Users/tatsuro/projects/hotel-scraping/test.csv")
-df=df[["宿泊人数","眺望"]]
-row_list = df.iloc[0, :].tolist()
-print(row_list)
+data = [list(e) for e in zip(df['宿泊人数'],df['眺望'],df["お部屋の設備"],df["バスルームの設備"])]
+print(data)
 
 # クラス呼び出し
 app = Flask(__name__)
@@ -16,7 +15,12 @@ def hello_World():
 
 @app.route('/hello')
 def hello():
-    return render_template("test.html",username="taro",data=row_list)
+    return render_template("test.html",username="taro",data=data)
+
+@app.route('/test2')
+def hello():
+    return render_template("test2.html",username="taro",data=data)
+
 
 # サーバー起動
 app.run(debug=True)
